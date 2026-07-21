793d5b6aafb32eb82da0e96a6d7ac4b753d38b1b
gaborsch <gabor.schermann@gmail.com>
2026-07-21 10:20:16 +0200

    porting from Java, step 3

--- solver/State.go
+++ solver/State.go
@@ -2,8 +2,8 @@ package solver
 
 type state struct {
 	board           *board
-	setValuemoves   []Move
-	clearFloatmoves []Move
+	setValueMoves   []Move
+	clearFloatMoves []Move
 }
 
 func new_state() state {
@@ -12,10 +12,39 @@ func new_state() state {
 }
 
 func (s *state) addMoves(moves []Move) {
-	// TODO: moves.forEach(this::addMove);
+	for _, m := range moves {
+		s.addMove(m)
+	}
 	for _, m := range moves {
 		if m.moveType == moveType_INIT_VALUE {
 			s.board.setFixedValue(int(m.pos), uint32(m.value))
 		}
 	}
+}
+
+func (s *state) addMove(m Move) {
+	switch m.moveType {
+	case moveType_INIT_VALUE:
+		s.setValueMoves = append(s.setValueMoves, m)
+	case moveType_SET_VALUE:
+		s.setValueMoves = append(s.setValueMoves, m)
+	case moveType_CLEAR_FLOAT:
+		s.clearFloatMoves = append(s.clearFloatMoves, m)
+	}
+}
+
+func (s *state) hasNextMove() bool {
+	return len(s.setValueMoves) > 0 || len(s.clearFloatMoves) > 0
+}
+
+func (s *state) getNextMove() Move {
+	var m Move
+	if len(s.setValueMoves) > 0 {
+		m = s.setValueMoves[0]
+		s.setValueMoves = s.setValueMoves[1:]
+	} else {
+		m = s.clearFloatMoves[0]
+		s.clearFloatMoves = s.clearFloatMoves[1:]
+	}
+	return m
 }
